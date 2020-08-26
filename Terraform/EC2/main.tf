f1312aeca12a5e28b9fc0013d1b1578d9b48c7d9
JSidat <JSidat@qa.com>
2020-08-26 14:06:55 +0100

    installations

--- Terraform/EC2/main.tf
+++ Terraform/EC2/main.tf
@@ -5,7 +5,18 @@ resource "aws_instance" "web_server" {
     vpc_security_group_ids = var.vpc_security_group_ids
     subnet_id = var.public_subnet_id
     associate_public_ip_address = var.associate_public_ip_address
-    user_data = var.user_data
+    user_data = data.template_file.installing_jenkins.rendered
+    
+    tags = {
+        Name = "Web_server"
+}
+
+}
+
+data "template_file" "installins" {
+  template = file("C:/Users/Junaid/OneDrive/QA academy/SFIA2_DEPLOYMENT/sfia2-deployment/sfia2-deployment/Terraform/EC2/installations.sh")
+}   
+
 
 
 tags = {
