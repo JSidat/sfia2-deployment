7828f1c72d1eac4f3f10ceb7b580fe23a26da062
JSidat <JSidat@qa.com>
2020-09-01 13:54:57 +0100

    add ansible installation script

--- Terraform/EC2/installations.sh
+++ Terraform/EC2/installations.sh
@@ -9,15 +9,6 @@ sudo apt-get install jenkins -y
 
 
 
-
-
-# Installing Ansible
-sudo apt install python3-pip -y
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-pip3 install --user ansible 
-
 #AWS CLI Install
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
@@ -27,7 +18,7 @@ sudo ./aws/install
 
 #install terraform 
 
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 
 # Aquire the terraform tools.
 wget https://releases.hashicorp.com/terraform/0.12.28/terraform_0.12.28_linux_amd64.zip
@@ -45,3 +36,5 @@ rm terraform_*_linux_*.zip
 echo ""
 
 terraform --version
+
+sh C:/Users/Junaid/OneDrive/QA academy/SFIA2_DEPLOYMENT/sfia2-deployment/sfia2-deployment/Terraform/EC2/ansible.sh
