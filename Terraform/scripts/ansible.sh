99db2e435c8a0bf3044fdb447bdad5740c167bc0
Ubuntu <ubuntu@ip-10-10-1-174.eu-west-1.compute.internal>
2020-09-02 13:04:18 +0000

    deleted duplicate folders

--- Terraform/scripts/ansible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Installing Ansible
-sudo apt install python3-pip -y
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-pip3 install --user ansible 
